78058e23fee07b60601e366cff0f569b5820a075
mkorman9 <m.korman94@gmail.com>
2017-07-09 22:18:14 +0200

    working version

--- genetics/evolution.py
+++ genetics/evolution.py
@@ -3,71 +3,67 @@ import random
 
 import numpy as np
 
-POPULATION_SIZE = 20
-BEST_INDIVIDUALS_TO_TAKE = 3
-MUTATION_CHANCE = 0.05
+POPULATION_SIZE = 200
+BEST_INDIVIDUALS_TO_TAKE = 2
+MUTATION_CHANCE = 0.001
 
 
 class Solver(object):
     def __init__(self, blueprint: np.array):
         self._blueprint = blueprint
-        self._current_best_sample = Sample(shape=blueprint.shape)
-
-        print("Initial loss: {}".format(self._current_best_sample.get_loss(blueprint)))
+        self._current_best_sample = None
+        self._population = [Sample(shape=blueprint.shape) for _ in range(0, POPULATION_SIZE)]
 
     def step(self) -> np.array:
-        population = self._create_population()
-        best_individuals = self._resolve_best_individuals(population)
-        effect = self._perform_crossing(best_individuals)
+        best_score = self._current_best_sample.get_loss(self._blueprint) if self._current_best_sample else float('inf')
+        self._sort_population_by_best_scores()
+        best_sample = self._perform_crossing_and_get_best()
 
-        if effect.get_loss(self._blueprint) < self._current_best_sample.get_loss(self._blueprint):
-            self._current_best_sample = effect
+        if best_sample.get_loss(self._blueprint) < best_score:
+            self._current_best_sample = best_sample
 
-        print('Current loss: {}'.format(self._current_best_sample.get_loss(self._blueprint)))
+        print('Loss: {}'.format(best_score))
 
+        self._mutate_population()
         return self._current_best_sample.get_state()
 
-    def _create_population(self):
-        population = []
-        for individual in range(POPULATION_SIZE):
-            sample = Sample(self._current_best_sample.get_state())
-            sample.perform_random_mutation()
-            population.append(sample)
-        return population
+    def _mutate_population(self):
+        for i in range(POPULATION_SIZE):
+            self._population[i].perform_random_mutation()
 
-    def _resolve_best_individuals(self, population):
-        ranking = sorted(
-            population,
-            key=lambda individual: individual.get_loss(self._blueprint),
-            reverse=True
+    def _sort_population_by_best_scores(self):
+        self._population = sorted(
+            self._population,
+            key=lambda individual: individual.get_loss(self._blueprint)
         )
 
-        return ranking[:BEST_INDIVIDUALS_TO_TAKE]
-
-    def _perform_crossing(self, best_individuals):
-        current = best_individuals[0]
+    def _perform_crossing_and_get_best(self):
+        best_individuals = self._population[:BEST_INDIVIDUALS_TO_TAKE]
+        best_genome_sample = best_individuals[0]
         for i in range(1, len(best_individuals)):
-            current = current.perform_crossing(best_individuals[i])
-        return current
+            best_genome_sample = best_genome_sample.perform_crossing(best_individuals[i])
+
+        for i in range(BEST_INDIVIDUALS_TO_TAKE, POPULATION_SIZE):
+            self._population[i] = self._population[i].perform_crossing(best_genome_sample)
+
+        return best_genome_sample
 
 
 class Sample(object):
     def __init__(self, parent: np.array=None, shape: Tuple[int, int]=None):
-        self._state = np.copy(parent) if parent is not None else np.zeros(shape)
+        self._state = np.copy(parent) if parent is not None else np.random.random(shape)
 
     def get_state(self) -> np.array:
         return self._state
 
     def get_loss(self, blueprint: np.array):
         return np.sqrt(
-            np.sum((self._state - blueprint) ** 2)
+            np.sum(
+                (self._state - blueprint) ** 2
+            )
         )
 
     def perform_random_mutation(self):
-        #for pixel in np.nditer(self._state, op_flags=['readwrite']):
-        #    if random.random() <= MUTATION_CHANCE:
-        #        pixel[...] = random.random()
-
         color = random.random()
         max_w, max_h = self._state.shape
         x, y = random.randint(0, max_w), random.randint(0, max_h)
@@ -75,7 +71,7 @@ class Sample(object):
 
         for i in range(x, min(x + w, max_w)):
             for j in range(y, min(y + h, max_h)):
-                self._state[i, j] = color
+                self._state[i, j] = (self._state[i, j] + color) / 2
 
     def perform_crossing(self, other_sample: 'Sample') -> 'Sample':
         return Sample((self._state + other_sample._state) / 2)

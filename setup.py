53e25163e2fc75e1d6facb8fe034be068fc75d0e
mkorman9 <m.korman94@gmail.com>
2017-07-12 19:38:29 +0200

    project files

--- setup.py
+++ setup.py
@@ -3,11 +3,20 @@ from setuptools import setup
 with open('requirements.txt') as f:
     requirements = f.readlines()
 
-version = '0.1'
+version = 'dynamic-dev'
 
 setup(
     name='agios',
     packages=['agios'],
     version=version,
+    description='Simple genetic algorithms framework for Python',
+    author='Michal Korman',
+    author_email='m.korman94@gmail.com',
+    url='https://github.com/mkorman9/agios',
+    download_url='https://github.com/mkorman9/framepy/agios/{}'.format(version),
+    keywords='ai genetic algorithms',
+    classifiers=[
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ],
     install_requires=[requirement for requirement in requirements if len(requirement) > 0]
 )

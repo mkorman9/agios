4c695cc697d5e99f5de15b0b89a1395678019a2f
mkorman9 <m.korman94@gmail.com>
2017-07-16 12:51:06 +0200

    release metadata

--- setup.py
+++ setup.py
@@ -5,21 +5,25 @@ def read(file_path):
     with open(file_path) as f:
         return [line for line in f.readlines() if len(line) > 0]
 
-version = 'dynamic-dev'
+version = '2017.7.1'
 
 setup(
     name='agios',
     packages=['agios'],
     version=version,
     description='Simple genetic algorithms framework for Python',
+    long_description='\n'.join(read('README.md')),
     author='Michal Korman',
     author_email='m.korman94@gmail.com',
+    license='MIT',
     url='https://github.com/mkorman9/agios',
     download_url='https://github.com/mkorman9/agios/tarball/{}'.format(version),
     keywords='ai genetic algorithms',
     classifiers=[
-        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Programming Language :: Python :: 3'
     ],
+    python_requires='>=3',
     install_requires=read('requirements.txt'),
     tests_require=read('test-requirements.txt')
 )
